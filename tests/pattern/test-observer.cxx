29a3e20dac69f4cef4de645fd829c51613058084
Tsukumo <tsukumokun@icloud.com>
2014-03-28 00:30:35 -0400

    Created a test for the observer pattern
    Basic test creates a concrete subject and observer and
    attempts to send data through the subject via notify
    after attaching the observer.

---
+++ tests/pattern/test-observer.cxx
@@ -0,0 +1,47 @@
+/******************************************************************************
+ * 'arc' - is a c++ framework for generic development. This framework         *
+ *         offers a diverse range of tools and algorithms as the base         *
+ *         for any project.                                                   *
+ *                                                                            *
+ *                             Copyright (C) 2014                             *
+ *                Christopher Kelley <tsukumokun(at)icloud.com>               *
+ *                                                                            *
+ * This work is licensed under the                                            *
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0               *
+ * International License. To view a copy of this license, visit               *
+ * http://creativecommons.org/licenses/by-nc-nd/4.0/deed.en_US.               *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       *
+ ******************************************************************************/
+
+#include "../../arc/pattern/observer.hxx"
+
+#include <iostream>
+
+class ConcreteObserver : public arc::Observer
+{
+    void onNotify(arc::P_ObserverSubject sender,void *data)
+    {
+        std::cout << "Sender :" << sender << std::endl;
+        std::cout << "Data   :" << *static_cast<int *>(data) << std::endl;
+    }
+};
+
+class ConcreteSubject : public arc::ObserverSubject
+{
+};
+
+int main()
+{
+    arc::P_ObserverSubject subject(new ConcreteSubject());
+    arc::P_Observer observer(new ConcreteObserver());
+
+    subject->attach(observer);
+
+    int a = 2;
+    subject->notify(&a);
+
+    return 0;
+}
