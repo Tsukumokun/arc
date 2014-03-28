4576045d2458140c39bb8f4e37f2ed572c494e24
Tsukumo <tsukumokun@icloud.com>
2014-03-28 00:29:14 -0400

    Added observer pattern files
    Created implementation for the observer pattern.
    Also added these files to the make list. Will likely move
    these to external files if it is easy enough with cmake.

---
+++ arc/pattern/observer.cxx
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * 'arc' - is a c++ framework for generic development. This framework         *
+ *         offers a diverse range of tools and algorithms as the base         *
+ *         for any project.                                                   *
+ *                                                                            *
+ *                             Copyright (C) 2014                             *
+ *                Christopher Kelley <tsukumokun(at)icloud.com>               *
+ *                                                                            *
+ * This work is licensed under the                                            *
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0               *
+ * International License. To view a copy of this license, visit               *
+ * http://creativecommons.org/licenses/by-nc-nd/4.0/deed.en_US.               *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       *
+ ******************************************************************************/
+
+
+// External std library includes
+#include <list>
+
+
+// Internal project includes
+#include "observer.hxx"
+
+
+namespace arc
+{
+
+
+//---------- ObserverSubject::attach() -----------------------------------------
+void ObserverSubject::attach(P_Observer observer)
+{
+    this->m_observers.push_back(observer);
+}
+
+
+//---------- ObserverSubject::detach() -----------------------------------------
+void ObserverSubject::detach(P_Observer observer)
+{
+    this->m_observers.remove(observer);
+}
+
+
+//---------- ObserverSubject::notify() -----------------------------------------
+void ObserverSubject::notify(void *data)
+{
+    for (std::list<P_Observer>::const_iterator iter = this->m_observers.begin();
+         iter != this->m_observers.end(); iter++)
+    {
+        if (*iter != NULL)
+        {
+            (*iter)->onNotify(this->shared_from_this(),data);
+        }
+    }
+}
+
+
+}
---
+++ arc/pattern/observer.hxx
@@ -0,0 +1,88 @@
+/******************************************************************************
+ * 'arc' - is a c++ framework for generic development. This framework         *
+ *         offers a diverse range of tools and algorithms as the base         *
+ *         for any project.                                                   *
+ *                                                                            *
+ *                             Copyright (C) 2014                             *
+ *                Christopher Kelley <tsukumokun(at)icloud.com>               *
+ *                                                                            *
+ * This work is licensed under the                                            *
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0               *
+ * International License. To view a copy of this license, visit               *
+ * http://creativecommons.org/licenses/by-nc-nd/4.0/deed.en_US.               *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       *
+ ******************************************************************************/
+
+#pragma once
+
+// External std library includes
+#include <list>
+
+// External other library includes
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/shared_ptr.hpp>
+
+namespace arc
+{
+
+/**
+ * @class Observer
+ * @brief Parent class for observers looking at a subject.
+ *
+ * Observer pattern implementation follows details outlined
+ * at http://en.wikipedia.org/wiki/Observer_pattern.
+ */
+class Observer;
+/**
+ * @var typedef boost::shared_ptr<Observer> P_Observer
+ * @brief Definition for an observer pointer.
+ */
+typedef boost::shared_ptr<Observer> P_Observer;
+
+/**
+ * @class Observer
+ * @brief Parent class for observer subjects being looked at.
+ *
+ * Observer subject pattern implementation follows details outlined
+ * at http://en.wikipedia.org/wiki/Observer_pattern.
+ */
+class ObserverSubject;
+/**
+ * @var typedef boost::shared_ptr<ObserverSubject> P_ObserverSubject
+ * @brief Definition for an observer subject pointer.
+ */
+typedef boost::shared_ptr<ObserverSubject> P_ObserverSubject;
+
+//========== Observer ==========================================================
+class Observer : public boost::enable_shared_from_this<Observer>
+{
+public:
+
+    /**
+     * @brief Notifies the observer of a change in the subject.
+     *
+     * Called by the notification function of an observer
+     * subject to notify this observer that something has changed.
+     *
+     * @param sender Shared pointer to an observer subject.
+     * @param data   Raw pointer to some data, unspecified type.
+     */
+    virtual void onNotify(P_ObserverSubject sender, void *data) = 0;
+};
+
+//========== ObserverSubject ===================================================
+class ObserverSubject : public boost::enable_shared_from_this<ObserverSubject>
+{
+private:
+    std::list<P_Observer> m_observers; //!< List of observers
+public:
+    void attach(P_Observer observer);
+    void detach(P_Observer observer);
+    void notify(void *data);
+};
+
+
+}
